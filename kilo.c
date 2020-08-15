adb6bbd69f75e654f21b9ff8549c6e020863a45c
Renan M Galvão <renan.m.galvao@gmail.com>
2020-08-15 11:19:03 -0300

    Enabled raw mode

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -13,7 +15,10 @@ void enableRawMode(){
 	atexit(disableRawMode);
 
 	struct termios raw = orig_termios;
-	raw.c_lflag &= ~(ECHO | ICANON);
+	raw.c_iflag &= ~(BRKINT| INPCK| ISTRIP| IXON | ICRNL);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -21,6 +26,12 @@ int main(){
 	enableRawMode();
 
 	char c;
-	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+		if(iscntrl(c)){
+			printf("%d\r\n", c);
+		}else{
+			printf("%d ('%c')\r\n", c, c);
+		}
+	}
 	return 0;
 }
